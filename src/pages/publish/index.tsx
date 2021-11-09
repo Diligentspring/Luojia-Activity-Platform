bfe78d545103707457c19b1d67a69392d248ae02
Isabel1015 <48252032+Isabel1015@users.noreply.github.com>
2021-11-09 23:20:47 +0800

    feat: publish表单校验

--- src/pages/publish/index.tsx
+++ src/pages/publish/index.tsx
@@ -3,50 +3,69 @@ import { Button, Card, Col, DatePicker, Form, Input, InputNumber, Row, message }
 import { publishApplication } from '@/services/publish';
 import Activity, { ActivityProps } from '@/components/Activity';
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
 
 const Publish = () => {
   const [publishForm] = Form.useForm<ActivityProps>();
+
+  const disabledDate = (current: any) => {
+    return current && current < moment().endOf('day');
+  };
 
   return (
     <PageContainer>
       <Card style={{ padding: 20, width: '80%' }}>
         <Form form={publishForm}>
-          <Form.Item name="title" label="活动名称" required>
+          <Form.Item name="title" label="活动名称" required rules={[{ required: true }]}>
             <Input></Input>
           </Form.Item>
           <Row gutter={64}>
             <Col>
-              <Form.Item label="活动开始时间" name="time_start" required>
-                <DatePicker showTime={true} format={'YYYY-MM-DD hh:mm:ss'}></DatePicker>
+              <Form.Item
+                label="活动开始时间"
+                name="time_start"
+                required
+                rules={[{ required: true }]}
+              >
+                <DatePicker
+                  showTime={true}
+                  format={'YYYY-MM-DD hh:mm:ss'}
+                  disabledDate={disabledDate}
+                />
               </Form.Item>
             </Col>
             <Col>
-              <Form.Item name="time_end" label="活动结束时间" required>
-                <DatePicker showTime={true}></DatePicker>
+              <Form.Item name="time_end" label="活动结束时间" required rules={[{ required: true }]}>
+                <DatePicker showTime={true} disabledDate={disabledDate} />
               </Form.Item>
             </Col>
           </Row>
           <Row gutter={64}>
             <Col>
-              <Form.Item name="start" label="报名开始时间" required>
-                <DatePicker showTime={true}></DatePicker>
+              <Form.Item name="start" label="报名开始时间" required rules={[{ required: true }]}>
+                <DatePicker showTime={true} disabledDate={disabledDate} />
               </Form.Item>
             </Col>
             <Col>
-              <Form.Item name="deadline" label="报名结束时间" required>
-                <DatePicker showTime={true}></DatePicker>
+              <Form.Item name="deadline" label="报名结束时间" required rules={[{ required: true }]}>
+                <DatePicker showTime={true} disabledDate={disabledDate} />
               </Form.Item>
             </Col>
           </Row>
           <Row gutter={185} style={{ width: '100%' }}>
             <Col>
-              <Form.Item name="number_people" label="招募人数" required>
+              <Form.Item
+                name="number_people"
+                label="招募人数"
+                required
+                rules={[{ required: true }]}
+              >
                 <InputNumber />
               </Form.Item>
             </Col>
           </Row>
 
-          <Form.Item name="site" label="活动地点" required>
+          <Form.Item name="site" label="活动地点" required rules={[{ required: true }]}>
             <Input required />
           </Form.Item>
           <Form.Item name="introduction" label="活动描述">
@@ -60,19 +79,39 @@ const Publish = () => {
             onClick={async () => {
               const formValue = publishForm.getFieldsValue(true);
 
-              const res = await publishApplication({
-                data: {
-                  ...formValue,
-                  time_start: formValue['time_start']?.format?.('YYYY-MM-DD HH:mm:ss'),
-                  time_end: formValue['time_end']?.format?.('YYYY-MM-DD HH:mm:ss'),
-                  start: formValue['start']?.format?.('YYYY-MM-DD HH:mm:ss'),
-                  deadline: formValue['deadline']?.format?.('YYYY-MM-DD HH:mm:ss'),
-                },
-              });
-              if (res.code === 1) {
-                message.success('活动发布成功!');
+              // 校验必填项
+              await publishForm.validateFields([
+                'title',
+                'time_start',
+                'time_end',
+                'site',
+                'number_people',
+                'deadline',
+                'start',
+              ]);
+
+              // 校验时间顺序
+              if (
+                formValue['start'].unix() < formValue['deadline'].unix() &&
+                formValue['deadline'].unix() < formValue['time_start'].unix() &&
+                formValue['time_start'].unix() < formValue['time_end'].unix()
+              ) {
+                const res = await publishApplication({
+                  data: {
+                    ...formValue,
+                    time_start: formValue['time_start']?.format?.('YYYY-MM-DD HH:mm:ss'),
+                    time_end: formValue['time_end']?.format?.('YYYY-MM-DD HH:mm:ss'),
+                    start: formValue['start']?.format?.('YYYY-MM-DD HH:mm:ss'),
+                    deadline: formValue['deadline']?.format?.('YYYY-MM-DD HH:mm:ss'),
+                  },
+                });
+                if (res.code === 1) {
+                  message.success('活动发布成功!');
+                } else {
+                  message.error('活动发布失败, 请重试!');
+                }
               } else {
-                message.error('活动发布失败, 请重试!');
+                message.error('活动时间顺序有误, 请重试!');
               }
             }}
           >
