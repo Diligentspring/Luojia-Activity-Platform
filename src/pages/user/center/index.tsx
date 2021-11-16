0795bcc59f21f7f4c40cac3de2e3ff4c0103c99a
TanZheling <tanzheling@foxmail.com>
2021-11-16 16:14:55 +0800

    救救窝之半成品头像上传ww

--- src/pages/user/center/index.tsx
+++ src/pages/user/center/index.tsx
@@ -1,16 +1,41 @@
 import { useState } from 'react';
-import { Avatar, Button, Card } from 'antd';
+import {Upload, Avatar, Button, Card,Image } from 'antd';
 import background from '../../../../public/background.jpg';
+import avat from '../../../../public/avatar.png';
 import styles from './index.less';
-import { CalendarOutlined, MailOutlined, MobileOutlined } from '@ant-design/icons';
+import {UploadOutlined, CalendarOutlined, MailOutlined, MobileOutlined } from '@ant-design/icons';
 import PageContainer from '@/components/PageContainer';
 import CenterCard from './components/CenterCard';
 import InfoEditCard from './components/InfoEditCard';
-import { useModel } from 'umi';
+import {FormattedMessage, useModel } from 'umi';
+import ImgCrop from 'antd-img-crop';
 
 const Center = () => {
-  const { initialState } = useModel('@@initialState');
+  const { initialState,setInitialState } = useModel('@@initialState');
   const [isEditing, setIsEditing] = useState<boolean>(false);
+  console.log(initialState?.currentUser?.avatar);
+  const AvatarView = (props: any) => (
+    
+    <>
+      
+      <div className={styles.avatar}>
+        <Image src={props.avatar} fallback={avat} width={200}
+      height={200} />
+      
+      </div>
+      <Upload showUploadList={false} action="/api/uploadavatar" onChange={props.onChange}>
+        <div className={styles.button_view}>
+          <Button>
+            <UploadOutlined />
+            <FormattedMessage
+              id="usermanagementandpersonalandaccountsettings.basic.change-avatar"
+              defaultMessage="更换头像"
+            />
+          </Button>
+        </div>
+      </Upload>
+    </>
+  );
 
   return (
     <PageContainer>
@@ -18,12 +43,18 @@ const Center = () => {
         <Card className={styles.header}>
           <img alt="backPic" src={background} style={{ width: '100%', height: 300 }} />
           <div className={styles.headerContent}>
-            <Avatar
-              src={initialState?.currentUser?.avatar}
+            <AvatarView classname={styles.right}
+              avatar={initialState?.currentUser?.avatar}
               shape="square"
               size="large"
-              className={styles.avatar}
-            ></Avatar>
+              onChange={(info: any) => {
+                if (info.file.status === 'done') {
+                  setInitialState({...initialState, 
+                    currentUser:{...initialState?.currentUser, avatar: '/api' + info.file.response.imgUrl}})
+                  
+                }
+              }}
+            />
             <div className={styles.briefInfoDiv}>
               <div className={styles.userName}>{initialState?.currentUser?.username}</div>
               <div className={styles.otherInfo}>
